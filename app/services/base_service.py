abea9622518da6ebd25dcf0f06cfe1c5a9287d36
SHASHA184 <sashashelehan@gmail.com>
2024-11-29 14:57:25 +0200

    update base service class

--- app/services/base_service.py
+++ app/services/base_service.py
@@ -7,18 +7,52 @@ from app.services.redis_service import RedisService
 
 class BaseService(ABC):
     def __init__(self, db: AsyncSession):
+        """
+        Base service class for CRUD operations.
+        Attributes:
+            db (AsyncSession): Async SQLAlchemy session.
+            redis_service (RedisService): Redis service instance.
+            entity_name (str): Name of the entity.
+        """
         self.db = db
         self.redis_service = RedisService()
+        self.entity_name = self.__class__.__name__.replace("Service", "").lower()
 
     @abstractmethod
     async def get(self, skip: int = 0, limit: int = 10):
+        """
+        Get all entities.
+        Args:
+            skip (int): Number of entities to skip.
+            limit (int): Maximum number of entities to return.
+        Returns:
+            List of SQLAlchemy model instances.
+        """
         pass
 
     @abstractmethod
     async def get_one(self, id: int, use_cache=True):
+        """
+        Get a single entity by ID.
+        Args:
+            id (int): Entity ID.
+            use_cache (bool): If True, use the cache.
+        Returns:
+            SQLAlchemy model instance.
+        """
         pass
 
     async def get_entity_or_404(self, model, id):
+        """
+        Get an entity by ID or raise a 404 error if it doesn't exist.
+        Args:
+            model: SQLAlchemy model.
+            id (int): Entity ID.
+        Returns:
+            SQLAlchemy model instance.
+        Raises:
+            EntityNotFoundException: If the entity doesn't exist.
+        """
         query = select(model).filter(model.id == id)
         entity = await self.db.execute(query)
         entity = entity.scalars().first()
@@ -26,14 +60,42 @@ class BaseService(ABC):
             raise EntityNotFoundException(model.__name__)
         return entity
 
+    def get_cache_key(self, id: int = None, is_list: bool = False, **kwargs) -> str:
+        """
+        Generate cache key dynamically.
+        Args:
+            id (int): Entity ID.
+            is_list (bool): If True, generate a list cache key.
+            kwargs: Additional parameters for list cache key (e.g., pagination).
+        Returns:
+            str: Redis cache key.
+        """
+        if is_list:
+            list_key_parts = [f"{key}:{value}" for key, value in kwargs.items()]
+            return f"{self.entity_name}s:{':'.join(list_key_parts)}"
+        return f"{self.entity_name}:{id}"
+
     @abstractmethod
     async def create(self, obj):
+        """Create an entity."""
         pass
 
     @abstractmethod
     async def update(self, id: int, obj):
+        """Update an entity"""
         pass
 
     @abstractmethod
     async def delete(self, id: int):
+        """Delete an entity."""
         pass
+
+    async def invalidate_entity_cache(self, id: int):
+        """Invalidate a single entity's cache."""
+        cache_key = self.get_cache_key(id=id)
+        await self.redis_service.clear_cache_by_key(cache_key)
+
+    async def invalidate_list_cache(self):
+        """Invalidate all list caches for the entity."""
+        list_pattern = f"{self.entity_name}s:*"
+        await self.redis_service.clear_cache_by_pattern(list_pattern)
--- app/services/product_service.py
+++ app/services/product_service.py
@@ -8,7 +8,7 @@ from app.services.base_service import BaseService
 
 class ProductService(BaseService):
     async def get(self, skip: int = 0, limit: int = 10):
-        cache_key = f"products:{skip}:{limit}"
+        cache_key = self.get_cache_key(is_list=True, skip=skip, limit=limit)
 
         cached_products = await self.redis_service.get_json(cache_key)
 
@@ -26,15 +26,16 @@ class ProductService(BaseService):
         return products
 
     async def get_one(self, id: int, use_cache=True):
+        cache_key = self.get_cache_key(id=id)
         if use_cache:
-            cached_product = await self.redis_service.get_json(f"product:{id}")
+            cached_product = await self.redis_service.get_json(cache_key)
 
             if cached_product:
                 return cached_product[0]
 
         product = await self.get_entity_or_404(ProductModel, id)
         await self.redis_service.set_json(
-            f"product:{id}", Product.model_validate(product).model_dump_json()
+            cache_key, Product.model_validate(product).model_dump_json()
         )
         return product
 
@@ -42,12 +43,13 @@ class ProductService(BaseService):
         db_product = ProductModel(**obj.model_dump())
         await db_product.save(self.db)
 
+        cache_key = self.get_cache_key(id=db_product.id)
         await self.redis_service.set_json(
-            f"product:{db_product.id}",
+            cache_key,
             Product.model_validate(db_product).model_dump_json(),
         )
 
-        await self.invalidate_product_list_cache()
+        await self.invalidate_list_cache()
 
         return db_product
 
@@ -56,9 +58,10 @@ class ProductService(BaseService):
         await db_product.update(self.db, **obj.dict(exclude_unset=True))
 
         updated_product = Product.model_validate(db_product).model_dump_json()
-        await self.redis_service.set_json(f"product:{id}", updated_product)
+        cache_key = self.get_cache_key(id=id)
+        await self.redis_service.set_json(cache_key, updated_product)
 
-        await self.invalidate_product_list_cache()
+        await self.invalidate_list_cache()
 
         return db_product
 
@@ -66,12 +69,7 @@ class ProductService(BaseService):
         product = await self.get_one(id, use_cache=False)
         await product.delete(self.db)
 
-        await self.redis_service.clear_cache_by_key(f"product:{id}")
-        
-        await self.invalidate_product_list_cache()
+        await self.invalidate_entity_cache(id)
+        await self.invalidate_list_cache()
 
         return product
-
-    async def invalidate_product_list_cache(self):
-        """Invalidate all product list caches."""
-        await self.redis_service.clear_cache_by_pattern("products:*")
